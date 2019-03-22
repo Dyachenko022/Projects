6c64dd5cdf33e71c767faa5ebe278e501af3e257
Dyachenko022 <47686417+Dyachenko022@users.noreply.github.com>
2019-03-22 14:32:20 +0300

    Update complex.h

--- complex.h
+++ complex.h
@@ -1,13 +1,22 @@
-#pragma once
-#ifndef COMPLEX_H
-#define COMPLEX_H
-struct complex
-{
-public:
-	int re1, im1, re2, im2;
-	int sumRe = 0;
-	int sumIm = 0;
-	void init(int a, int b, int c, int d, int x, int y);
 
-}complexx;
+struct complex
+
+{
+
+public:
+
+	int re1, im1, re2, im2;
+
+	int sumRe = 0;
+
+	int sumIm = 0;
+
+	void summ(int a, int b, int c, int d);
+	void substract(int a, int b, int c, int d, int x, int y);
+	void multiply(int a, int b, int c, int d, int x, int y);
+
+
+
+};
+
 #endif
